927b0efbf402b187740924a5f5fc66fc39c09b3e
Eduardo Destruti <limaedu@gmail.com>
2015-03-02 02:06:20 -0300

    organize code and insert html

--- Slim/Model/Worship.php
+++ Slim/Model/Worship.php
@@ -22,7 +22,7 @@ class Worship
     {
         $result = $this->church->findOne(array('_id' => new \MongoId($_id)));
 
-        echo '<br/><center><a href="https://github.com/limaedu/webservicelayer" target="_blank">See on Github<a/></center><br/><br/>';
+        echo '<br/><center><a href="https://github.com/limaedu/webservicelayer" target="_blank"><img src="http://webservicelayer.com/img/github.png" /><a/></center>';
         echo '<br/><center><a href="/remove">Remove All<a/></center><br/><br/>';
 
         foreach ($result as $key => $value) {
@@ -46,7 +46,7 @@ class Worship
 
         try {
 
-            echo '<br/><center><a href="https://github.com/limaedu/webservicelayer" target="_blank">See on Github<a/></center><br/><br/>';
+            echo '<br/><center><a href="https://github.com/limaedu/webservicelayer" target="_blank"><img src="http://webservicelayer.com/img/github.png" /><a/></center>';
 
             if ($this->church->count() == 0){
 
