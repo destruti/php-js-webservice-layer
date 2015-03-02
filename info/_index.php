22d0357174a67716d6b9698ca06673c14660c90b
Eduardo Destruti <limaedu@gmail.com>
2015-03-02 01:10:34 -0300

    organize code and insert html

---
+++ info/_index.php
@@ -0,0 +1,24 @@
+<?php
+
+require '../vendor/autoload.php';
+
+\Slim\Slim::registerAutoloader();
+
+//$app = new \Slim\Slim();
+$app = new \Slim\Slim(array( 'debug' => true ));
+
+$worship = new \Slim\Model\Worship();
+
+$app->get('/', function () use ($worship) { $worship->getAllWorships(); });
+
+$app->get('/church', function () use ($worship) { $worship->getAllWorships(); });
+
+$app->get('/worship/:_id', function ($_id) use ($worship) { $worship->viewWorship($_id); });
+
+$app->post('/addWorship', function () use ($worship) { $worship->addWorship(); });
+
+$app->put('/updateWorship', function () use ($worship) { $worship->updateWorship(); });
+
+$app->delete('/deleteWorship', function () use ($worship) { $worship->deleteWorship(); });
+
+$app->run();
--- public/index.php
+++ public/index.php
@@ -1,24 +1,151 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>Web Service Layer</title>
+		<meta name="description" content="Some inspiration for web divider styles" />
+		<meta name="keywords" content="separator css, divider css, separator style, web design, flat design, inspiration, responsive, svg, pseudo-elements" />
+		<meta name="author" content="Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/icons.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<script src="js/modernizr.custom.js"></script>
+	</head>
+	<body>
+		<div class="container">
 
-require '../vendor/autoload.php';
+			<!-- Top Navigation -->
+			<header>
+				<h1>
+					Web Service Layer
+					<span>some kind of organization calls on the web</span>
+				</h1>
+			</header>
 
-\Slim\Slim::registerAutoloader();
+			<section class="col-2 ss-style-triangles">
+				<div class="column text">
+					<h2>a new way to organize your calls on the web</h2>
+					<p>
+						a new way to organize your calls on the web
+						a new way to organize your calls on the web
+						a new way to organize your calls on the web
+					</p>
+				</div>
+				<div class="column">
+					<span class="icon icon-headphones"></span>
+				</div>
+			</section>
 
-//$app = new \Slim\Slim();
-$app = new \Slim\Slim(array( 'debug' => true ));
 
-$worship = new \Slim\Model\Worship();
+			<section class="color">
+				<h2>which is a service layer?</h2>
+				<p>
+					which is a service layer? which is a service layer? which is a service layer?
+					which is a service layer? which is a service layer? which is a service layer?
+					which is a service layer? which is a service layer? which is a service layer?
+					which is a service layer? which is a service layer? which is a service layer?
+					which is a service layer? which is a service layer? which is a service layer?
+					which is a service layer? which is a service layer? which is a service layer?
+				</p>
+			</section>
 
-$app->get('/', function () use ($worship) { $worship->getAllWorships(); });
 
-$app->get('/church', function () use ($worship) { $worship->getAllWorships(); });
-
-$app->get('/worship/:_id', function ($_id) use ($worship) { $worship->viewWorship($_id); });
-
-$app->post('/addWorship', function () use ($worship) { $worship->addWorship(); });
-
-$app->put('/updateWorship', function () use ($worship) { $worship->updateWorship(); });
-
-$app->delete('/deleteWorship', function () use ($worship) { $worship->deleteWorship(); });
-
-$app->run();
+			<section class="col-3 ss-style-doublediagonal">
+				<div class="column">
+					<span class="icon icon-home"></span>
+					<p>
+						datalayer datalayer datalayer datalayer datalayer datalayer
+						datalayer datalayer datalayer datalayer datalayer datalayer
+						datalayer datalayer datalayer datalayer datalayer datalayer
+					</p>
+				</div>
+				<div class="column">
+					<span class="icon icon-bullhorn"></span>
+					<p>
+						domainlayer domainlayer domainlayer domainlayer domainlayer
+						domainlayer domainlayer domainlayer domainlayer domainlayer
+						domainlayer domainlayer domainlayer domainlayer domainlayer
+					</p>
+				</div>
+				<div class="column">
+					<span class="icon icon-blog"></span>
+					<p>
+						servicelayer servicelayer servicelayer servicelayer servicelayer
+						servicelayer servicelayer servicelayer servicelayer servicelayer
+						servicelayer servicelayer servicelayer servicelayer servicelayer
+					</p>
+				</div>
+			</section>
+			<section class="col-2 ss-style-halfcircle">
+				<div class="column">
+					<span class="icon icon-images"></span>
+				</div>
+				<div class="column text">
+					<h2>we are always online</h2>
+					<p>
+						we are always online we are always online we are always online we are always online we are always online
+						we are always online we are always online we are always online we are always online we are always online
+						we are always online we are always online we are always online we are always online we are always online
+					</p>
+				</div>
+			</section>
+			<section class="color ss-style-bigtriangle">
+				<h2>we focus on the security of your information</h2>
+				<p>
+					we focus on the security of your information we focus on the security of your information
+					we focus on the security of your information we focus on the security of your information
+					we focus on the security of your information we focus on the security of your information
+					we focus on the security of your information we focus on the security of your information
+				</p>
+			</section>
+			<svg id="bigTriangleShadow" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
+				<path id="trianglePath1" d="M0 0 L50 100 L100 0 Z" />
+				<path id="trianglePath2" d="M50 100 L100 40 L100 0 Z" />
+			</svg>
+			<section>
+				<h2>we use nosql database</h2>
+				<p>
+					we use nosql database we use nosql database we use nosql database
+					we use nosql database we use nosql database we use nosql database
+					we use nosql database we use nosql database we use nosql database
+					we use nosql database we use nosql database we use nosql database
+					we use nosql database we use nosql database we use nosql database
+					we use nosql database we use nosql database we use nosql database
+				</p>
+			</section>
+			<svg id="clouds" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
+				<path d="M-5 100 Q 0 20 5 100 Z
+						 M0 100 Q 5 0 10 100
+						 M5 100 Q 10 30 15 100
+						 M10 100 Q 15 10 20 100
+						 M15 100 Q 20 30 25 100
+						 M20 100 Q 25 -10 30 100
+						 M25 100 Q 30 10 35 100
+						 M30 100 Q 35 30 40 100
+						 M35 100 Q 40 10 45 100
+						 M40 100 Q 45 50 50 100
+						 M45 100 Q 50 20 55 100
+						 M50 100 Q 55 40 60 100
+						 M55 100 Q 60 60 65 100
+						 M60 100 Q 65 50 70 100
+						 M65 100 Q 70 20 75 100
+						 M70 100 Q 75 45 80 100
+						 M75 100 Q 80 30 85 100
+						 M80 100 Q 85 20 90 100
+						 M85 100 Q 90 50 95 100
+						 M90 100 Q 95 25 100 100
+						 M95 100 Q 100 15 105 100 Z">
+				</path>
+			</svg>
+			<section class="related">
+				<p>quem somos?</p>
+				<p><a href="http://destruti.com/">Eduardo Destruti</a></p>
+				<p><a href="https://maquinadobem.com">Máquina do Bem</a></p>
+			</section>
+		</div><!-- /container -->
+	</body>
+</html>
